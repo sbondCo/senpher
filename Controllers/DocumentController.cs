5e3c74224d62d7c6a7c0ee2a788930a3f7f86a0b
IRHM <37304121+IRHM@users.noreply.github.com>
2021-07-31 21:48:20 +0100

    RecieveFile in working order and Get returning FileContentResult

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB;
+using Microsoft.AspNetCore.StaticFiles;
 using MongoDB.Driver;
 using Senpher.Models;
 using Senpher.Services;
@@ -16,50 +16,60 @@ namespace Senpher.Controllers
   public class DocumentController : ControllerBase
   {
     [HttpGet("{id}")]
-    public Document Get(string id)
+    public async Task<FileResult> Get(string id)
     {
       var db = Database.Get();
       var col = db.GetCollection<Document>("files");
-      Console.WriteLine(col);
 
-      // return col.Find(_ => true).ToList();
+      Document doc = (Document)await (await col.FindAsync(x => x.ID == id, new FindOptions<Document> { Limit = 1 }))
+        .FirstOrDefaultAsync();
 
-      return (Document)col.Find(x => x.ID == id);
+      var path = Path.Join("./uploaded-files", doc.ID, doc.Name);
+      new FileExtensionContentTypeProvider().TryGetContentType(path, out var contentType);
+      contentType ??= "application/unknown";
 
-      // return new File
-      // {
-      //   Name = $"grobgroop {id}"
-      // };
+      return new FileContentResult(System.IO.File.ReadAllBytes(path), contentType)
+      {
+        FileDownloadName = doc.Name
+      };
     }
 
-    [HttpPost]
-    public async Task<string> RecieveFile()
+    [HttpPost("{name}")]
+    public async Task<Document> RecieveFile(string name)
     {
-      Console.WriteLine($"ContentType: {Request.ContentType}");
-      Console.WriteLine($"ContentLength: {Request.ContentLength}");
+      foreach (var h in Request.Headers)
+      {
+        Console.WriteLine(h);
+      }
 
+      var doc = new Document
+      {
+        Name = name
+      };
+
+      // Add to DB
+      var db = Database.Get();
+      var col = db.GetCollection<Document>("files");
+      await col.InsertOneAsync(doc);
+
+      // Make sure request can be read multiple times
       Request.EnableBuffering();
 
+      // Read stream
       StreamReader sr = new StreamReader(Request.Body, Encoding.UTF8, true, 4096);
       var content = await sr.ReadToEndAsync();
 
-      StreamWriter sw = new StreamWriter("./uploaded-file", false, Encoding.UTF8, 4096);
+      // Write stream to file
+      var outDir = $"./uploaded-files/{doc.ID}";
+      Directory.CreateDirectory(outDir);
+      StreamWriter sw = new StreamWriter(Path.Join(outDir, doc.Name), false, Encoding.UTF8, 4096);
       sw.Write(content);
       sw.Close();
 
       // Reset position in stream
       Request.Body.Position = 0;
 
-      // Add to DB
-      var db = Database.Get();
-      var col = db.GetCollection<Document>("files");
-
-      await col.InsertOneAsync(new Document
-      {
-        Name = "hi"
-      });
-
-      return content;
+      return doc;
     }
   }
 }
